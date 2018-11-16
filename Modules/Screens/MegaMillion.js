d7ddc9a76826eae7bc002cda045eab165aa5dc42
SiddharthChoudhary <firesid12@gmail.com>
2018-11-15 19:46:33 -0500

    MegaMillion done perfectly
    It's working perfectly

--- Modules/Screens/MegaMillion.js
+++ Modules/Screens/MegaMillion.js
@@ -6,11 +6,88 @@ class MegaMillion extends Component{
       headerTitle:'MegaMillion'
     }
   }
+  constructor(props){
+  super(props)
+  this.state = {
+    isLoaderOn:true,
+    megaMillionArray:null,
+    showArray:false
+  }
+  }
+  
+  saveThisNumber(){
+    const shareArray  = JSON.stringify(this.state.megaMillionArray);
+    Share.share({
+      message: shareArray
+    })
+  }
+
+  showRandomNumber(){
+      return(
+          <View>
+              <Text style={styles.numberText}>{JSON.stringify(this.state.megaMillionArray)}</Text>
+              <Button
+                  style={styles.textBorder}
+            color= {Platform.OS ==='ios'?'#ffffff':'#c80512'}
+                  onPress={() => this.saveThisNumber() }
+                  title="Save"
+              />
+              <View style={styles.label}></View>
+              <Button
+                  style={styles.textBorder}
+            color= {Platform.OS ==='ios'?'#ffffff':'#c80512'}
+            onPress={() => this.props.navigation.goBack() }
+                  title="Back to generate new Number"
+              />
+          </View>
+      )
+  }
+  getMegaMillionNumbers(){
+    fetch('http://quest.phy.stevens.edu:5050/megaMillion')
+    .then((response) => response.json())
+    .then((responseJson) => {
+        this.setState({
+            isLoaderOn: false,
+            megaMillionArray: responseJson.finalrandomarray,
+            showArray : true
+        });
+    })
+  }
   render(){
     return(
-    <View>
+    <View style={[styles.container,styles.themeBg]}>
+    {
+      (this.state.isLoaderOn)?
+      this.getMegaMillionNumbers():
+      this.showRandomNumber()
+    }
     </View>
   )
   }
 }
+const styles= StyleSheet.create({
+  numberText :{
+      padding : 15,
+      borderWidth : 1,
+      marginBottom : 20,
+      borderColor : '#fff',
+      color :'#fff'
+  },
+  label : {
+      marginTop : 20,
+      marginBottom : 10,
+      color : '#fff'
+  },
+   themeBg :{
+        backgroundColor : '#c80512',
+        margin : 0,
+        padding : 0
+    },
+    container : {
+        flex : 1,
+        margin : 15,
+        alignItems :'center',
+        justifyContent : 'center'
+    },
+})
 export default MegaMillion
